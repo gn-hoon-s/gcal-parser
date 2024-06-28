e48402caf088b8e35002c7f88bb229d8acf86f10
Hoon Shin <hoon.s@goodnotesapp.com>
2024-06-28 10:49:07 +0800

    all changes

---
+++ calendar-test/calendar_testApp.swift
@@ -0,0 +1,18 @@
+//
+//  calendar_testApp.swift
+//  calendar-test
+//
+//  Created by Hoon Shin on 26/6/2024.
+//
+
+import SwiftUI
+import SwiftyChrono
+
+@main
+struct calendar_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
